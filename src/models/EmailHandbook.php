4cd5603a1938746d43b6464dcd34851969c949ef
citizenzet <exgamer@live.ru>
2019-11-19 10:25:55 +0600

    добавил комменты к методам и классам

--- src/models/EmailHandbook.php
+++ src/models/EmailHandbook.php
@@ -19,7 +19,7 @@ class EmailHandbook extends ActiveRecord
      */
     public static function tableName()
     {
-        return '{{%user_role}}';
+        return '{{%email_handbook}}';
     }
 
     /**
