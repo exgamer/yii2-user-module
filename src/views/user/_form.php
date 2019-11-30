e200acd54dde2edc2d522fd4a3527e9153fcdc0c
Olzhas Kulzhambekov <exgamer@live.ru>
2019-11-30 17:00:28 +0600

    dfs

--- src/views/user/_form.php
+++ src/views/user/_form.php
@@ -8,30 +8,40 @@ use kamaelkz\yii2admin\v1\widgets\formelements\editors\froala\FroalaEditor;
 
 <?php Pjax::begin(['formSelector' => '#user-form']); ?>
 
-    <?php $form = ActiveForm::begin(['id' => 'user-form']); ?>
-    <div class="card">
-        <div class="card-body text-right">
-            <?=  Html::submitButton(
-                '<b><i class="icon-checkmark3"></i></b>' . Yii::t('yii2admin', 'Сохранить'),
-                [
-                    'class' => 'btn bg-success btn-labeled btn-labeled-left ml-1'
-                ]
-            ); ?>
-        </div>
-
-
-
-        <?= $form->field($model, 'username')->textInput(['maxlength' => true]) ?>
-        <?= $form->field($model, 'locale')->dropDownList(
-            Yii::$app->localeService->catalog(),
+<?php $form = ActiveForm::begin(['id' => 'user-form']); ?>
+<div class="card">
+    <div class="card-body text-right">
+        <?=  Html::submitButton(
+            '<b><i class="icon-checkmark3"></i></b>' . Yii::t('yii2admin', 'Сохранить'),
             [
-                'prompt' => Yii::t('backend', 'Выберите язык')
+                'class' => 'btn bg-success btn-labeled btn-labeled-left ml-1'
             ]
-        );?>
-        <div class="form-group">
-            <?= Html::submitButton(Yii::t('user', 'Сохранить'), ['class' => 'btn btn-success']) ?>
-        </div>
-
-        <?php ActiveForm::end(); ?>
+        ); ?>
     </div>
+    <div class="card-body">
+        <div class="row">
+            <div class="col-lg-6 col-md-6 col-sm-12">
+                <?= $form->field($model, 'username')->textInput(['maxlength' => true]) ?>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-12">
+                <?= $form
+                    ->field($model, 'locale')
+                    ->dropDownList(Yii::$app->localeService->catalog(), [
+                        'class' => 'form-control custom-select',
+                        'prompt' => Yii::t('backend', 'Выберите язык')
+                    ]);
+                ?>
+            </div>
+        </div>
+    </div>
+    <div class="card-body text-right">
+        <?=  Html::submitButton(
+            '<b><i class="icon-checkmark3"></i></b>' . Yii::t('yii2admin', 'Сохранить'),
+            [
+                'class' => 'btn bg-success btn-labeled btn-labeled-left ml-1'
+            ]
+        ); ?>
+    </div>
+    <?php ActiveForm::end(); ?>
+</div>
 <?php Pjax::end(); ?>
