703e588aae286ff222cd97fdf00d81ead785044c
citizenzet <exgamer@live.ru>
2019-12-03 16:20:35 +0600

    добавил комменты к методам и классам

--- src/views/user/create.php
+++ src/views/user/create.php
@@ -3,7 +3,7 @@
 $this->setTitle(Yii::t('yii2admin', 'Новая запись'));
 $this->pushBreadcrumbs(['label' => $model::label(), 'url' => ['index']]);
 $this->pushBreadcrumbs($this->title);
-$this->viewHelper()->pushPageHeader(['index'], Yii::t('yii2admin', 'Пoльзователи'),'icon-list');
+$this->viewHelper()->pushPageHeader(['index'], $model::label(),'icon-list');
 ?>
 <?= $this->render('_form', [
     'model' => $model,
