316a28f2b1b489891fe438939b840af2b2c8bdc5
citizenzet <exgamer@live.ru>
2019-12-03 17:15:45 +0600

    добавил комменты к методам и классам

--- src/views/user/update.php
+++ src/views/user/update.php
@@ -5,7 +5,7 @@ $this->pushBreadcrumbs(['label' => $model::label(), 'url' => ['index']]);
 $this->pushBreadcrumbs($this->title);
 $this->viewHelper()->pushPageHeader();
 $this->viewHelper()->pushPageHeader(['view', 'id' => $originModel->id], Yii::t('yii2admin', 'Просмотр'),'icon-file-eye2');
-$this->viewHelper()->pushPageHeader(['index'], Yii::t('yii2admin', 'Пользователи'),'icon-list');
+$this->viewHelper()->pushPageHeader(['index'], $model::label(),'icon-list');
 ?>
 <?= $this->render('_form', [
     'model' => $model,
