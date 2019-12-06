cff5a749a48ecfcf43a156cad97d07efafc5c0dc
citizenzet <exgamer@live.ru>
2019-12-06 12:13:16 +0600

    добавил комменты к методам и классам

---
+++ src/services/traits/UserSupportTrait.php
@@ -0,0 +1,37 @@
+<?php
+namespace concepture\yii2user\services\traits;
+
+use Yii;
+use concepture\yii2logic\forms\Model;
+
+/**
+ * Trait UserSupportTrait
+ * @package concepture\yii2user\services\traits
+ */
+trait UserSupportTrait
+{
+    /**
+     * Устанавливает текущего пользователя
+     * @param Model $model
+     * @param bool $ignoreSetted
+     */
+    protected function setCurrentUser(Model $model, $ignoreSetted = true)
+    {
+        if (! $ignoreSetted){
+            return;
+        }
+
+        if (! Yii::$app->has('user')){
+            $model->user_id = 1;
+
+            return;
+        }
+
+        if ($model->user_id){
+            return;
+        }
+
+        $model->user_id = Yii::$app->user->identity->id;
+    }
+}
+
