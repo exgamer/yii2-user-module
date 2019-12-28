7590d1eb42ce5b98e8a31cbaa329573769facdcb
Olzhas Kulzhambekov <exgamer@live.ru>
2019-12-28 15:32:10 +0600

    dfs

--- src/services/UserAccountOperationService.php
+++ src/services/UserAccountOperationService.php
@@ -1,8 +1,14 @@
 <?php
 namespace concepture\yii2user\services;
 
+use concepture\yii2logic\models\ActiveRecord;
 use concepture\yii2logic\services\Service;
 use concepture\yii2logic\services\traits\StatusTrait;
+use concepture\yii2user\enum\UserAccountOperationTypeEnum;
+use concepture\yii2user\forms\UserAccountForm;
+use concepture\yii2user\forms\UserAccountOperationForm;
+use concepture\yii2user\traits\ServicesTrait;
+use Exception;
 
 /**
  * Class UserAccountOperationService
@@ -12,14 +18,70 @@ use concepture\yii2logic\services\traits\StatusTrait;
 class UserAccountOperationService extends Service
 {
     use StatusTrat;
+    use ServicesTrait;
 
-    public function refill($sum)
+    /**
+     * Пополнение счета
+     *
+     * @param integer $user_id
+     * @param double $sum
+     * @param integer $currency
+     * @return ActiveRecord
+     */
+    public function refill($user_id, $sum, $currency)
     {
+        $account = $this->userAccountOperationService()->getOneByCondition([
+            'user_id' => $user_id,
+            'currency' => $currency,
+        ]);
+        if (! $account){
+            $accountForm = new UserAccountForm();
+            $accountForm->user_id = $user_id;
+            $accountForm->currency = $currency;
+            $accountForm->status = 1;
+            $account = $this->userAccountService()->create($accountForm);
+        }
 
+        $form = new UserAccountOperationForm();
+        $form->type = UserAccountOperationTypeEnum::REFILL;
+        $form->currency = $currency;
+        $form->sum = $sum;
+        $form->status = 1;
+        $form->account_id = $account->id;
+
+        return $this->userAccountOperationService()->create($form);
     }
 
-    public function writeOff($sum)
+    /**
+     * Снятие со счета
+     *
+     * @param integer $user_id
+     * @param double $sum
+     * @param integer $currency
+     * @return ActiveRecord
+     * @throws Exception
+     */
+    public function writeOff($user_id, $sum, $currency)
     {
+        $account = $this->userAccountOperationService()->getOneByCondition([
+            'user_id' => $user_id,
+            'currency' => $currency,
+        ]);
+        if (! $account){
+            throw new Exception("account not exists");
+        }
 
+        if ($account->balance < $sum){
+            throw new Exception("not enough balance");
+        }
+
+        $form = new UserAccountOperationForm();
+        $form->type = UserAccountOperationTypeEnum::WRITE_OFF;
+        $form->currency = $currency;
+        $form->sum = $sum;
+        $form->status = 1;
+        $form->account_id = $account->id;
+
+        return $this->userAccountOperationService()->create($form);
     }
 }
