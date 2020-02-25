adcdd4bcebd743f60f2a22f19314cf29697d4d4d
Olzhas Kulzhambekov <exgamer@live.ru>
2020-02-25 16:37:52 +0600

    +

---
+++ src/helpers/SsoHelper.php
@@ -0,0 +1,38 @@
+<?php
+namespace concepture\yii2user\helpers;
+
+use concepture\yii2logic\helpers\JwtHelper;
+use Yii;
+use yii\helpers\Url;
+
+/**
+ * Class SsoHelper
+ * @package common\helpers
+ * @author Olzhas Kulzhambekov <exgamer@live.ru>
+ */
+class SsoHelper
+{
+    public static function getSignInUrl()
+    {
+        $host = Url::home(YII_DEBUG?'http':'https');
+
+        return Yii::$app->params['SSO_HOST']. "/api/auth/sign-in?redirect={$host}";
+    }
+
+    public static function getCheckoutUrl()
+    {
+        return Yii::$app->params['SSO_HOST']. "/checkout?token=" . static::getSsoJwtToken();
+    }
+
+    public static function getSsoJwtToken()
+    {
+        $payload = [
+            'app_id' => Yii::$app->params['SSO_APP_ID']
+        ];
+        if (! Yii::$app->user->isGuest){
+            $payload['user_id'] = Yii::$app->user->identity->id;
+        }
+
+        return JwtHelper::getJWT($payload);
+    }
+}
