8d92e2e75ed912d0d45d052a4acdd62f2ce8b668
Olzhas Kulzhambekov <exgamer@live.ru>
2020-02-28 17:13:09 +0600

    social auth

--- src/services/UserSocialAuthService.php
+++ src/services/UserSocialAuthService.php
@@ -4,16 +4,12 @@ namespace concepture\yii2user\services;
 use concepture\yii2logic\services\traits\StatusTrait;
 use concepture\yii2user\forms\UserSocialAuthForm;
 use yii\db\ActiveQuery;
-use concepture\yii2user\enum\UserCredentialStatusEnum;
-use concepture\yii2user\enum\UserCredentialTypeEnum;
-use concepture\yii2user\forms\UserCredentialForm;
 use concepture\yii2user\forms\UserEmailCredentialForm;
 use concepture\yii2logic\forms\Model;
 use concepture\yii2logic\models\ActiveRecord;
 use concepture\yii2logic\services\Service;
 use Yii;
 use concepture\yii2user\traits\ServicesTrait;
-use concepture\yii2handbook\services\traits\ReadSupportTrait as HandbookReadSupportTrait;
 
 /**
  * Class UserSocialAuthService
