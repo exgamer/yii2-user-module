b512830c67d38c2d93f7151e47246417276d1d55
Eugene Poletaev <eugene@media.bet>
2020-04-07 17:49:08 +0600

    merge access rules

--- src/web/controllers/Controller.php
+++ src/web/controllers/Controller.php
@@ -4,6 +4,10 @@ namespace concepture\yii2user\web\controllers;
 
 use concepture\yii2user\enum\UserRoleEnum;
 use kamaelkz\yii2admin\v1\controllers\BaseController;
+use kamaelkz\yii2admin\v1\modules\audit\actions\AuditAction;
+use kamaelkz\yii2admin\v1\modules\audit\actions\AuditRollbackAction;
+use kamaelkz\yii2admin\v1\modules\audit\services\AuditService;
+use yii\helpers\ArrayHelper;
 
 /**
  * Базовый контроллер для модуля пользователя
@@ -14,14 +18,64 @@ use kamaelkz\yii2admin\v1\controllers\BaseController;
  */
 abstract class Controller extends BaseController
 {
+    /**
+     * @param \yii\base\Action $action
+     * @return bool
+     * @throws \yii\web\BadRequestHttpException
+     */
+    public function beforeAction($action)
+    {
+        if (
+            $action->id == 'update'
+            && method_exists($action->controller, 'getService')
+            && AuditService::isAuditAllowed($modelClass = $action->controller->getService()->getRelatedModelClass())
+        ) {
+            $this->getView()->viewHelper()->pushPageHeader(
+                [AuditAction::actionName(), 'id' => \Yii::$app->request->get('id')],
+                \Yii::t('yii2user', 'Аудит'),
+                'icon-eye'
+            );
+        }
+        return parent::beforeAction($action);
+    }
+
+    /**
+     * @return array
+     */
     protected function getAccessRules()
     {
-        return [
+        return ArrayHelper::merge(
+            parent::getAccessRules(),
             [
-                'actions' => ['index', 'create','update', 'view','delete'],
-                'allow' => true,
-                'roles' => [UserRoleEnum::ADMIN],
-            ]
-        ];
+                [
+                    'actions' => ['index', 'create','update', 'view','delete'],
+                    'allow' => true,
+                    'roles' => [UserRoleEnum::ADMIN],
+                ],
+                [
+                    'actions' => [
+                        AuditAction::actionName(),
+                        AuditRollbackAction::actionName(),
+                    ],
+                    'allow' => true,
+                    'roles' => [
+                        // TODO change
+                        UserRoleEnum::ADMIN,
+                    ],
+                ],
+            ],
+        );
+    }
+
+    /**
+     * @return array
+     */
+    public function actions()
+    {
+        $actions = parent::actions();
+        return ArrayHelper::merge($actions,[
+            AuditAction::actionName() => AuditAction::class,
+            AuditRollbackAction::actionName() => AuditRollbackAction::class,
+        ]);
     }
 }
