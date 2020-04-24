7024dd59b5ee8c0c56d0c9cab323dc19d2bc3744
Olzhas Kulzhambekov <exgamer@live.ru>
2020-04-24 12:31:08 +0600

    jwt

---
+++ src/components/twig/JwtHelperExtension.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace concepture\yii2user\components\twig;
+
+use concepture\yii2user\helpers\SsoHelper;
+use Yii;
+use Twig\TwigFunction;
+use Twig\Extension\AbstractExtension;
+
+/**
+ * Class JwtHelperExtension
+ * @package concepture\yii2user\components\twig
+ * @author Olzhas Kulzhambekov <exgamer@live.ru>
+ */
+class JwtHelperExtension extends AbstractExtension
+{
+    /**
+     * @return TwigFunction[]
+     */
+    public function getFunctions()
+    {
+        return [
+            new TwigFunction('jwt_token', function(array $array) {
+                return SsoHelper::getSsoJwtToken($array);
+            }),
+        ];
+    }
+}
