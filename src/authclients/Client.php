8c78cf3d07d19afa6237eab8d8079f760e543279
Olzhas Kulzhambekov <exgamer@live.ru>
2020-04-27 13:51:07 +0600

    jwt

---
+++ src/authclients/Client.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace concepture\yii2user\authclients;
+
+use yii\authclient\BaseClient;
+use yii\authclient\OAuth2;
+
+/**
+ * Class Instagram
+ * @package concepture\yii2user\authclients
+ * @author Olzhas Kulzhambekov <exgamer@live.ru>
+ */
+class Client extends BaseClient
+{
+    protected function initUserAttributes()
+    {
+
+    }
+}
