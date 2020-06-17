818827469933ae8dc547a8a70e507c1f89909e13
Olzhas Kulzhambekov <exgamer@live.ru>
2020-06-17 11:04:22 +0600

    domain rbac

--- src/rbac/rules/DomainRule.php
+++ src/rbac/rules/DomainRule.php
@@ -1,11 +1,14 @@
 <?php
 namespace concepture\yii2user\rbac\rules;
 
+use Yii;
 use yii\rbac\Rule;
 
 /**
  * Проверка на доступ к домену
- * Переменную $params мы передаем сюда, когда вызываем \Yii::$app->user->can('role', $param = ['model' => $model]);
+ * Когда вызываем \Yii::$app->user->can('role'); Будет проверен текущий домен
+ * Переменную $params мы передаем сюда, когда вызываем \Yii::$app->user->can('role', $param = ['domain_id' => 2]);
+ * Если не передавать будет проверен текущий домен
  */
 class DomainRule extends Rule
 {
@@ -19,7 +22,14 @@ class DomainRule extends Rule
      */
     public function execute($user, $item, $params)
     {
+        $domainId = $params['domain_id'] ?? null;
+        if (! $domainId) {
+            $domainId = Yii::$app->domainService->getCurrentDomainId();
+        }
 
+//        if ($domainId !== 1) {
+//            return false;
+//        }
 
         return true;
     }
