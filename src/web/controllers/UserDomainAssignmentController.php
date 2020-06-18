0d2d3a9e601b38b1acfd10994fab91d1f11e677f
Olzhas Kulzhambekov <exgamer@live.ru>
2020-06-18 11:01:03 +0600

    domain rbac

--- src/web/controllers/UserDomainAssignmentController.php
+++ src/web/controllers/UserDomainAssignmentController.php
@@ -1,144 +1,145 @@
 <?php
 
-namespace concepture\yii2user\web\controllers;
-
-use concepture\yii2handbook\services\DomainService;
 use concepture\yii2logic\enum\AccessTypeEnum;
-use concepture\yii2user\search\UserAuthAssignmentSearch;
-use concepture\yii2user\services\RbacService;
-use concepture\yii2user\services\UserDomainAssignmentService;
-use frontend\search\post\PostIndexSearch;
-use Yii;
-use yii\data\ArrayDataProvider;
+use yii\helpers\Url;
+use yii\helpers\Html;
+use yii\grid\GridView;
 use yii\helpers\ArrayHelper;
-use yii\rbac\Item;
-use yii\web\NotFoundHttpException;
-use concepture\yii2handbook\services\EntityTypePositionSortService;
-use concepture\yii2logic\filters\AjaxFilter;
+use kamaelkz\yii2admin\v1\widgets\formelements\Pjax;
+use kamaelkz\yii2admin\v1\widgets\lists\grid\EditableColumn;
 use kamaelkz\yii2admin\v1\enum\FlashAlertEnum;
+use kamaelkz\yii2admin\v1\widgets\notifications\alert\Alert;
 
-/**
- * Class UserDomainAssignmentController
- * @package concepture\yii2user\web\controllers
- * @author Olzhas Kulzhambekov <exgamer@live.ru>
- */
-class UserDomainAssignmentController extends Controller
-{
-    /**
-     * @return UserDomainAssignmentService
-     */
-    public function userDomainAssignmentService()
-    {
-        return Yii::$app->userDomainAssignmentService;
-    }
+$this->setTitle(Yii::t('yii2admin', 'Доступные версии'));
+$this->pushBreadcrumbs(['label' => Yii::t('yii2admin', 'Список пользователей'), 'url' => ['user/index']]);
+$this->pushBreadcrumbs($this->title);
+$this->viewHelper()->pushPageHeader(['user/index'], Yii::t('yii2admin', 'Список пользователей'),'icon-list');
 
-    /**
-     * @return DomainService
-     */
-    public function domainService()
-    {
-        return Yii::$app->domainService;
-    }
+?>
+<?php Pjax::begin(); ?>
 
-    /**
-     * @inheritDoc
-     */
-    public function actions()
-    {
-        $actions = parent::actions();
-        unset($actions['index'], $actions['create'], $actions['update'], $actions['view'], $actions['delete']);
+    <div class="row">
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header header-elements-inline">
+                    <h5 class="card-title">
+                        <?= Yii::t('yii2admin', 'Список версий'); ?>
+                    </h5>
+                </div>
+            </div>
+            <?= GridView::widget([
+                'dataProvider' => $domainsDataProvider,
+                'searchVisible' => false,
+                'columns' => [
+                    [
+                        'attribute' => 'country_caption',
+                        'label' => Yii::t('yii2admin', 'Версия'),
+                    ],
+                    [
+                        'class' => 'yii\grid\ActionColumn',
+                        'dropdown' => false,
+                        'template' => '{create}',
+                        'buttons' => [
+                            'create' => function ($url, $model) use ($user_id) {
+                                return Html::a(
+                                    '<i class="icon-checkmark3"></i>',
+                                    [
+                                        '/user/user-domain-assignment/create',
+                                        'user_id' => $user_id,
+                                        'domain_id' => $model['domain_id'],
+                                        'access' => AccessTypeEnum::READ
+                                    ],
+                                    [
+                                        'class' => 'admin-action list-icons-item',
+                                        'title' => Yii::t('yii2admin', 'Назначить'),
+                                        'data-pjax-id' => 'list-pjax',
+                                        'data-pjax-url' => Url::current([], true),
+                                    ]
+                                );
+                            },
+                        ],
+                    ]
+                ]
+            ]); ?>
+        </div>
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header header-elements-inline">
+                    <h5 class="card-title">
+                        <?= Yii::t('yii2admin', 'Список доступных версий'); ?>
+                    </h5>
+                </div>
+            </div>
+            <?= GridView::widget([
+                'dataProvider' => $userDomainsDataProvider,
+                'searchVisible' => false,
+                'columns' => [
+                    [
+                        'attribute' => 'country_caption',
+                        'label' => Yii::t('yii2admin', 'Версия'),
+                    ],
+                    [
+                        'label'=> Yii::t('yii2admin', 'Текущий доступ'),
+                        'value'=>function($data) {
+                            return AccessTypeEnum::label($data['access']);
+                        }
+                    ],
+                    [
+                        'label'=> Yii::t('yii2admin', 'Смена доступа'),
+                        'format' => 'raw',
+                        'value'=>function($data) use ($user_id) {
+                            if ($data['access'] == AccessTypeEnum::READ) {
+                                return Html::a(
+                                    '<i class="icon-pen-plus"></i>',
+                                    ['/user/user-domain-assignment/create', 'user_id' => $user_id, 'domain_id' => $data['domain_id'], 'access' => AccessTypeEnum::WRITE],
+                                    [
+                                        'class' => 'admin-action list-icons-item',
+                                        'title' => Yii::t('backend', 'Дать доступ на редактирование'),
+                                        'data-pjax-id' => 'list-pjax',
+                                        'data-pjax-url' => Url::current([], true),
+                                        'data-swal' => Yii::t('yii2admin', 'Дать доступ на редактирование'),
+                                    ]
+                                );
+                            }
 
-        return $actions;
-    }
+                            if ($data['access'] == AccessTypeEnum::WRITE) {
+                                return Html::a(
+                                    '<i class="icon-pen-minus"></i>',
+                                    ['/user/user-domain-assignment/delete', 'user_id' => $user_id, 'domain_id' => $data['domain_id'], 'access' => AccessTypeEnum::WRITE],
+                                    [
+                                        'class' => 'admin-action list-icons-item',
+                                        'title' => Yii::t('backend', 'Удалить доступ для редактирования'),
+                                        'data-pjax-id' => 'list-pjax',
+                                        'data-pjax-url' => Url::current([], true),
+                                        'data-swal' => Yii::t('yii2admin' , 'Удалить доступ для редактирования'),
+                                    ]
+                                );
+                            }
+                        }
+                    ],
+                    [
+                        'class' => 'yii\grid\ActionColumn',
+                        'dropdown' => false,
+                        'template' => '{delete}',
+                        'buttons' => [
+                            'delete' => function ($url, $data) use ($user_id) {
+                                return Html::a(
+                                    '<i class="icon-cross2"></i>',
+                                    ['/user/user-domain-assignment/delete', 'user_id' => $user_id, 'domain_id' => $data['domain_id'], 'access' => $data['access']],
+                                    [
+                                        'class' => 'admin-action list-icons-item',
+                                        'title' => Yii::t('backend', 'Удалить'),
+                                        'data-pjax-id' => 'list-pjax',
+                                        'data-pjax-url' => Url::current([], true),
+                                        'data-swal' => Yii::t('yii2admin' , 'Удалить'),
+                                    ]
+                                );
+                            },
+                        ],
+                    ]
+                ]
+            ]); ?>
+        </div>
+    </div>
 
-    public function actionIndex($user_id)
-    {
-        $userDomains = $this->userDomainAssignmentService()->getAllByCondition(
-            [
-                'user_id' => $user_id
-            ],
-        );
-        if (is_array($userDomains)) {
-            $result = [];
-            foreach ($userDomains as $data) {
-                if (! isset($result[$data['domain_id']])) {
-                    $result[$data['domain_id']] = $data;
-                    continue;
-                }
-
-                if ($data->access == AccessTypeEnum::WRITE) {
-                    $result[$data['domain_id']] = $data;
-                }
-            }
-            $userDomains = array_values($result);
-        }
-
-        $userDomainsDataProvider = new ArrayDataProvider([
-            'allModels' =>  $userDomains,
-            'pagination' => [
-                'pageSize' => 30,
-            ],
-        ]);
-        $userDomains = ArrayHelper::index($userDomains, 'domain_id');
-        $domains = $this->domainService()->getDomainsData();
-        foreach ($domains as $id => $domain) {
-            if (isset($userDomains[$id])) {
-                unset($domains[$id]);
-                $userDomains[$id]->country_caption = $domain['country_caption'];
-            }
-        }
-
-        $domainsDataProvider = new ArrayDataProvider([
-            'allModels' =>  $domains,
-            'pagination' => [
-                'pageSize' => 30,
-            ],
-        ]);
-
-        return $this->render('index', [
-            'user_id' => $user_id,
-            'domainsDataProvider' => $domainsDataProvider,
-            'userDomainsDataProvider' => $userDomainsDataProvider,
-        ]);
-    }
-
-    /**
-     * @param $user_id
-     * @param $domain_id
-     * @param $access
-     * @return string
-     */
-    public function actionCreate($user_id, $domain_id, $access)
-    {
-        try {
-            if (($this->userDomainAssignmentService()->assign($user_id, $domain_id, $access)) !== false) {
-
-                return $this->responseNotify();
-            }
-        } catch (\Exception $e) {
-            return $this->responseNotify(FlashAlertEnum::WARNING, $e->getMessage());
-        }
-
-        return $this->responseNotify(FlashAlertEnum::WARNING, $this->getErrorFlash());
-    }
-
-    /**
-     * @param $user_id
-     * @param $domain_id
-     * @param $access
-     * @return string
-     */
-    public function actionDelete($user_id, $domain_id, $access)
-    {
-        try {
-            $result = $this->userDomainAssignmentService()->revoke($user_id, $domain_id, $access);
-            if ($result === false){
-                throw new \Exception(Yii::t('yii2admin', 'Невозможно удалить доступ !'));
-            }
-
-            return $this->responseNotify();
-        } catch (\Exception $e) {
-            return $this->responseNotify(FlashAlertEnum::WARNING, $e->getMessage());
-        }
-    }
-}
+<?php Pjax::end(); ?>
