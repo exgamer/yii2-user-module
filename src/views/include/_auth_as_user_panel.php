9bb0ba2d59c070bbfe05a6a098604035cd6b11de
Olzhas Kulzhambekov <exgamer@live.ru>
2020-10-16 15:05:48 +0600

    +

--- src/views/include/_auth_as_user_panel.php
+++ src/views/include/_auth_as_user_panel.php
@@ -1,5 +1,8 @@
 <div class="access-as-user">
-    <?= Yii::t('common', 'Вы просматриваете сайт как <br/> {user}', ['user' => $username])?>
+    <?= Yii::t('common', 'Вы просматриваете сайт как <br/> {user}' , ['user' => $username]); ?>
+</div>
+<div class="access-as-user-actions">
+    <?= \yii\helpers\Html::a(Yii::t('common', 'Выход'), ['site/logout'], ['class' => 'link' ,'style' => 'color: #CE272D;']); ?>
 </div>
 <style>
     .access-as-user{
@@ -15,7 +18,24 @@
         padding-left: 5px;
         padding-right: 5px;
         padding-top: 5px;
-        opacity: 0.5;
+        opacity: 0.7;
+        color: #000000;
+    }
+
+    .access-as-user-actions{
+        position: fixed;
+        right: 0;
+        top: 4rem;
+        display: flex;
+        height: 2rem;
+        background-color: #eee;
+        border-radius: 0 0 0 3px;
+        z-index: 2000;
+        text-align: center;
+        padding-left: 5px;
+        padding-right: 5px;
+        padding-top: 5px;
+        opacity: 0.7;
         color: #000000;
     }
 </style>
